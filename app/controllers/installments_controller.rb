c32943fc4452fa2e14bd202ea61478939ef73d73
Bruno <brunolimasud@gmail.com>
2016-06-27 11:27:28 -0300

    showing installment for each person

--- app/controllers/installments_controller.rb
+++ app/controllers/installments_controller.rb
@@ -1,7 +1,9 @@
 class InstallmentsController < ApplicationController
   def index
     @installments = Installment.select(:p_day, "SUM(value) as total_value").group(:p_day).order(:p_day)
+    
     @installments = Installment.for_pay_in_date(params[:p_day]) if params[:p_day].present?
+    @installments = Installment.for_pay_in_date_with_person_id(params[:p_day], params[:person_id]) if params[:p_day].present? && params[:person_id].present?
   end
 
   def new
--- app/models/installment.rb
+++ app/models/installment.rb
@@ -1,7 +1,12 @@
 class Installment < ActiveRecord::Base
   belongs_to :person
+  belongs_to :purchase
 
   scope :for_pay_in_date, ->(date) {
     select("installments.p_day", "installments.person_id", "SUM(value) as total_value").includes(:person).group("p_day, person_id").where(p_day: date)    
+  }
+
+  scope :for_pay_in_date_with_person_id, ->(date, person_id) {
+    where(p_day: date, person_id: person_id).includes(:person, :purchase)  
   }
 end
