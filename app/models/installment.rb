a4aefaacd73446dad92a061c671c71bf689b86eb
Bruno <brunolimasud@gmail.com>
2016-07-03 17:23:52 -0300

    Showing prev/next bills

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -12,10 +12,19 @@ class Installment < ActiveRecord::Base
   }
 
   scope :for_pay_in_date, ->(date) {
-    select("installments.p_day", "installments.person_id", "SUM(value) as total_value").includes(:person).group("p_day, person_id").where(p_day: date)    
+    select("installments.p_day", "installments.person_id", "SUM(value) as total_value").includes(:person).group("p_day, person_id").where(p_day: date).order("total_value desc")    
   }
 
   scope :for_pay_in_date_with_person_id, ->(date, person_id) {
-    where(p_day: date, person_id: person_id).includes(:person, :purchase)  
+    where(p_day: date, person_id: person_id).includes(:person, :purchase).order("purchases.purchased_in")
   }
+
+  def self.has_next? date
+     Installment.where(p_day: date+1.month).size > 0 ? true : false
+  end
+
+  def self.has_prev? date
+     Installment.where(p_day: date-1.month).size > 0 ? true : false
+  end
+
 end
