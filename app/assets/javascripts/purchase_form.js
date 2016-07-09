581b8e9834ce87723a8d5696f9a634ba8d944e7b
Bruno <brunolimasud@gmail.com>
2016-07-08 23:43:26 -0300

    fixing add value installment and calculate next value

--- app/assets/javascripts/purchase_form.js
+++ app/assets/javascripts/purchase_form.js
@@ -4,11 +4,21 @@ var removePersonFields = function(button){
 
 $(document).ready(function(){
 
+  var valuePurchaseField = document.getElementById('purchase-value');
   var addPersonElement = document.getElementById('add-person');
   var quantityPeople = 0
 
   addPersonElement.addEventListener('click', function() {
-    var fields = '<div id="fieldset-quantity-people-' + quantityPeople + '"><fieldset class="form-inline"><input id="person_id_' + quantityPeople + '" type="text" hidden="true" name=people[][id]><div class="form-group"><input type="text" class="form-control" name=people[][name] placeholder="Nome" data-autocomplete="purchases/autocomplete_person_name" data-id-element="#person_id_' + quantityPeople + '" > </div><div class="form-group"><div class="input-group"> <div class="input-group-addon">R$</div><input type="number" class="form-control" name=people[][value] placeholder="Valor"></div></div> <button id="quantity-people-' + quantityPeople + '" type="button" onclick="removePersonFields(this)" class="btn btn-primary remove-person">Remover</button></fieldset><br></div>';
+
+    totalValue = 0;
+          console.log('teste');
+    for (var i = 0; i < quantityPeople; i++) {
+      console.log('Valor total: value-person-' + i + '');
+      totalValue+=document.getElementById('value-person-' + i + '').value;
+    }
+
+    var nextValue = valuePurchaseField.value-totalValue;
+    var fields = '<div id="fieldset-quantity-people-' + quantityPeople + '"><fieldset class="form-inline"><input id="person_id_' + quantityPeople + '" type="text" hidden="true" name=people[][id]><div class="form-group"><input type="text" class="form-control" name=people[][name] placeholder="Nome" data-autocomplete="purchases/autocomplete_person_name" data-id-element="#person_id_' + quantityPeople + '" > </div><div class="form-group"><div class="input-group"> <div class="input-group-addon">R$</div><input type="text" id="value-person-' + quantityPeople + '" class="form-control" name=people[][value] placeholder="Valor" value=' + nextValue +'></div></div> <button id="quantity-people-' + quantityPeople + '" type="button" onclick="removePersonFields(this)" class="btn btn-primary remove-person">Remover</button></fieldset><br></div>';
 
     $("#fields-people").append(fields);
     quantityPeople++;
